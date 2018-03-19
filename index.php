e371849cb4fd9a84a526f42d762c9da8a853efe3
Losabit <quentin.clodion@gmail.com>
2018-03-19 18:20:22 +0100

    bande de pd

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 	<title>Viaxe</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -23,7 +23,7 @@
 		<div class="search container">
 			<form action="searchparcours" method="post">
 				Je recherche : <input type="radio" name="type" value="parcours" checked> Parcours
-				<input type="radio" name="type" value="people"> Utilisateur
+				<input type="radio" name="type" value="guide"> Guide
 				<br>
 				<br>
 				<input type="search" name="search" placeholder="Search...">
