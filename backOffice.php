a41d81fb47277e9c492ca98dddd5a07e39795222
Axodh <37446105+Axodh@users.noreply.github.com>
2018-03-19 01:09:51 +0100

    Backoffice

---
+++ backOffice.php
@@ -0,0 +1,39 @@
+<?php
+ 	require_once "functions.php";
+  include "header.php";
+	?>
+
+<!DOCTYPE html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="" content="">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	</head>
+
+
+	<div class="container">
+	<div class="row">
+	<div>
+	<table class="table">
+  <thead class="thead-inverse">
+    <br>
+    <br>
+    <br>
+    <tr>
+      <th>Id</th>
+      <th>Mail</th>
+      <th>Pseudo</th>
+    </tr>
+  </thead>
+
+
+
+
+  <tbody>
+		<?php  backOffice();	?>
+
+  </tbody>
+</table>
+</div>
+</div>
+</div>
---
+++ functions.php
@@ -0,0 +1,48 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "viaxe");
+define("DB_USER", "root");
+define("DB_PWD", "");
+
+function connectDb(){
+  try{
+    $db = new PDO('mysql:host=localhost;dbname=viaxe' , 'root' , '');
+  }catch(Exception $e){
+    die("Erreur SQL : ".$e->getMessage());
+  }
+return $db;
+}
+
+function backOffice(){
+
+  $db = connectDb();
+  $query = $db->prepare("SELECT Id,mail,pseudo FROM client");
+  $query->execute();
+
+  $result = $query->fetchAll();
+
+  foreach($result as $member){
+    echo'
+    <tr>
+      <td>'.$member["Id"].'</td>
+      <td>'.$member["mail"].'</td>
+      <td>'.$member["pseudo"].'</td>
+      <td>
+        <form method="DELETE" action="delete.php">
+          <input  name="id" type="text" value="'.$member["Id"].'"/>
+          <input type="submit" value="supprimer"/>
+        </form>
+      </td>
+    </tr>
+    ';
+
+  }
+}
+
+function deleteUser($Id){
+  $db = connectDb();
+  $query = $db->prepare("DELETE FROM client WHERE Id = :id");
+  $query->execute(["Id"=>$Id]);
+
+}
