5440adbe0edbc33bb22fcd1c27c1e82f0a04630e
Pierremar <jonasnizard@gmail.com>
2018-03-20 10:36:20 +0100

    Merge des fonction et correction sur le back office

--- include/functions.php
+++ include/functions.php
@@ -46,3 +46,17 @@ function deleteUser($Id){
   $query->execute(["Id"=>$Id]);
 
 }
+<?php
+
+function erreur($err='')
+{
+ $mess=($err!='')? $err:'Une erreur inconnue s\'est produite';
+ exit('<p>'.$mess.'</p>
+ <p>Cliquez <a href="./index.php">ici</a> pour revenir à la page d\'accueil</p></div></body></html>');
+}
+
+function chiffer ($password){
+  $salage='SuP4rS4aL4g3';
+  return hash('md5',$salage.$password);
+}
+ ?>
