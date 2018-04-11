d41932c06dbced55229e5dddc7f74bacd6bdc5a4
Pierremar <jonasnizard@gmail.com>
2018-04-11 20:18:15 +0200

    JS correction recheche parcours

---
+++ include/slider.js
@@ -0,0 +1,9 @@
+
+  var slider = document.getElementById("firstSlider");
+  var output = document.getElementById("price");
+  output.innerHTML = `Prix maximum : ${slider.value}`;
+
+  console.log(slider);
+  slider.oninput = function() {
+  output.innerHTML = `Prix maximum : ${slider.value}`;
+  }
