fb6e19298c547cc2ff7e7f6218c454c4125d429a
Axodh <37446105+Axodh@users.noreply.github.com>
2018-04-30 12:58:42 +0200

    Bawi

--- backOffice.php
+++ backOffice.php
@@ -11,8 +11,8 @@
 	?>
 
 <!DOCTYPE html>
+<html>
 	<head>
-    <html>
   		<meta charset="utf-8">
   		<meta name="" content="">
   		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
@@ -31,16 +31,16 @@
             </div>
             <div class="col-10">
             <nav class="navbar navbar-expand navbar-light fixed-top bg-light">
-              <a class="navbar-brand" href="#"> Viaxe </a>
+              <a class="navbar-brand" href="index.php"> Viaxe </a>
               <ul class="navbar-nav">
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Lundi</a>
+                  <a class="nav-link" href="#">Customer</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Mardi</a>
+                  <a class="nav-link" href="#">Guide</a>
                 </li>
                 <li class="nav-item">
-                 <a class="nav-link" href="#">Mercredi</a>
+                 <a class="nav-link" href="graphics.php">Graphs</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="#">Jeudi</a>
@@ -56,15 +56,15 @@
       <br>
       <div class="container">
       <h1> Customer Manager </h1>
-      <form>
+      <form method="POST" action="">
         <div class="form-row">
           <div class="col-md-4 mb-3">
             <label for="validationDefault01">Mail</label>
-            <input type="text" class="form-control" id="validationDefault01" placeholder="Mail" value="" required>
+            <input type="mail" class="form-control" id="validationDefault01" placeholder="Mail" value="mail" required>
           </div>
           <div class="col-md-4 mb-3">
             <label for="validationDefault02">Pseudo</label>
-            <input type="text" class="form-control" id="validationDefault02" placeholder="Pseudo" value="" required>
+            <input type="text" class="form-control" id="validationDefault02" placeholder="Pseudo" value="pseudo" required>
           </div>
           <div class="col-md-4 mb-3">
             <label for="validationDefaultUsername">Birthday</label>
@@ -96,12 +96,17 @@
       <table class="table">
         <thead>
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Mail</th>
+            <th scope="col">Pseudo</th>
+            <th scope="col">Birth</th>
+            <th scope="col">Gender</th>
+            <th scope="col">Banned</th>
+            <th scope="col">Tools</th>
+            <th scope="col">  </th>
           </tr>
         </thead>
+        <?php backOffice();
+        ?>
     </div>
     </body>
-  </html>
+</html>
